d38ec8a0cabd290ffe73654539bfb602b05b8d85
bzrimsek <brianzrimsek@gmail.com>
2026-07-08 07:49:28 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // madgolf — Service Worker vtouch
-const CACHE_NAME = 'madgolf-v0.90.93';
+const CACHE_NAME = 'madgolf-v0.90.94';
 const ASSETS = ['./', './index.html', './manifest.json', './logo.webp'];
 
 self.addEventListener('install', e => {
